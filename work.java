682affb7048067d38abcd5f07cbc13fdfe640404
Nt0ny <ntony@bk.ru>
2017-11-23 22:25:58 +0300

    Initial commit

---
+++ work.java
@@ -0,0 +1,20 @@
+package work;
+import java.util.Scanner;
+/* Test project for
+ * experiments wiht Github
+ * 23 11 2017
+  */
+
+public class work {
+
+	public static void main(String[] args) {
+		Scanner kbrd = new Scanner(System.in);
+		System.out.print("Num = ");
+		int x = kbrd.nextInt();
+		for (int i = 1; i <= x; i++) {
+			System.out.print(i+" ");
+			
+		}
+	}
+
+}
